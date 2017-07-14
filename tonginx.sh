45637d8a4d7efc82f565903745e43ff3062d1c46
zhongzhong <shuezhen.zhong@refordom.com>
2017-07-14 21:32:19 +0800

    修改脚本

--- tonginx.sh
+++ tonginx.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+echo "开始获取更新..."
 git pull
+echo "获取更新完成..."
+
+echo "开始安装node模块..."
 cnpm install
+echo "开始安装node模块..."
+
+echo "开始生成网站文件..."
 hexo generate
+echo "生成网站文件完成..."
+
+echo "开始复制文件..."
 rm -rf /usr/share/nginx/html/*
 cp -r /root/workspace/myblog/public/* /usr/share/nginx/html
+echo "复制完成..."
 echo "成功将public文件夹中的内容移动到nginx服务器中！"
